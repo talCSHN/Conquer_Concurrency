f3262fbbb0d0eed367c35ddd4ae89a55515fcae3
talCSHN <yujakinasakoon@gmail.com>
2025-06-10 23:02:46 +0900

    Update Program.cs

--- Concurrency_Self_Study/03_Async_Stream/Program.cs
+++ Concurrency_Self_Study/03_Async_Stream/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace _03_Async_Stream
+﻿
+namespace _03_Async_Stream
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async IAsyncEnumerable<string> GetValueAsync(HttpClient client)
         {
-            Console.WriteLine("Hello, World!");
+            int offset = 0;
+            const int limit = 10;
+            while (true)
+            {
+                // 전체 결과 중 현재 페이지를 가져와서 파싱
+                string result = await client.GetStringAsync(
+                    $"https://example.com/api/values?offset={offset}&limit={limit}");
+                string[] valuesOnThisPage = result.Split('\n');
+
+                // 현재 페이지의 결과 전달
+                foreach (string value in valuesOnThisPage )
+                {
+                    yield return value;
+                }
+
+                // 마지막 페이지면 끝냄
+                if (valuesOnThisPage.Length != limit)
+                {
+                    break;
+                }
+                // 아니면 다음 페이지로 넘어감
+                offset += limit;
+            }
+        }
+        public static async Task ProcessValueAsync(HttpClient client)
+        {
+            await foreach (string value in GetValueAsync(client).ConfigureAwait(false))
+            {
+                await Task.Delay(100).ConfigureAwait(false);  // 비동기 작업
+                Console.WriteLine(value);
+            }
+        }
+        
+
+        public static async IAsyncEnumerable<string> SlowRange()
+        {
+            throw new NotImplementedException();
         }
     }
 }
